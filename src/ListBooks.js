b4f5c45409825dee9fa5ba78cfc7dad4acc8abff
Josica <josica.ganesamurthy@gmail.com>
2017-09-24 12:46:00 -0400

    refactor: Test sectioning by mapping through static data

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,7 +3,41 @@ import React, { Component } from 'react'
 class ListBooks extends Component {
   render() {
     return (
-      
+      <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">Read</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid"> {this.props.books.map( (books) => (
+                <li>
+                  <div className="book">
+                    <div className="book-top">
+                      <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${books.coverURL}")` }}></div>
+                      <div className="book-shelf-changer">
+                        <select>
+                          <option value="none" disabled>Move to...</option>
+                          <option value="currentlyReading">Currently Reading</option>
+                          <option value="wantToRead">Want to Read</option>
+                          <option value="read">Read</option>
+                          <option value="none">None</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="book-title">{books.title}</div>
+                    <div className="book-authors">{books.authors}</div>
+                  </div>
+                </li>
+              ))}
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     )
   }
 }
