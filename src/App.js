6236e09022ef1e1dfd78c03e25486329fbf65519
Josica <josica.ganesamurthy@gmail.com>
2017-09-24 16:53:06 -0400

    refactor: Render UI with External BooksAPI

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import React, { Component } from 'react'
 // import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends Component {
   state = {
-    books: [
-      {
-        "id": "To Kill a Mockingbird",
-        "title": "To Kill a Mockingbird",
-        "authors": "Harper Lee",
-        "coverURL": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-        "shelf": "Currently Reading"
-      },
-      {
-        "id": "Ender's Game",
-        "title": "Ender's Game",
-        "authors": "Orson Scott Card",
-        "coverURL": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-        "shelf": "Want to Read"
-      },
-      {
-        "id": "Harry Potter and the Sorcerer's Stone",
-        "title": "Harry Potter and the Sorcerer's Stone",
-        "authors": "J.K. Rowling",
-        "coverURL": "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
-        "shelf": "Read"
-      }
-    ], 
+    books: [], 
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
@@ -36,6 +14,12 @@ class BooksApp extends Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
